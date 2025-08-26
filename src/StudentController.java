54f5b252541fdbd5d00361c198578bbe573adfbf
Rodrigo <rodrigodanielsalvasaccatoma@gmail.com>
2025-08-26 15:41:00 -0500

    Se hizo un cambio en la clase student

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,6 +1,6 @@
 public class StudentController {
 
     public void controller() {
-        System.out.println("Hola");
+        System.out.println("Hola contruccion y pruebas de software");
     }
 }
